3907566db63f8281a1f22b2d875eb709d508d7aa
Egor Astakhov <noise718@gmail.com>
2021-09-29 17:01:09 +0300

    Initial Commit

---
+++ Controls Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Controls Practice
+//
+//  Created by Егор Астахов on 29.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
